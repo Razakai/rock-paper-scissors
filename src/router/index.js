42413d6d65390e956143a22894b4b9e3f88304ee
Razakai <sean.a.holland@mycit.ie>
2020-11-05 20:52:48 +0000

    removed redundant code
    removed vue.js started crap ;)

--- src/router/index.js
+++ src/router/index.js
@@ -1,24 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
-// import Home from '../views/Home.vue'
-// import about from '../views/About.vue'
 import game from '../pages/game/game.vue'
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: game
-  },
-  /* {
-    path: '/about',
-    name: 'About',
-    component: about
-  },
-  {
-    path: '/game',
     name: 'game',
     component: game
-  }, */
+  },
   {
     path: '/:catchAll(.*)',
     redirect: '/'
